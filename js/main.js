dd98d68a63503cafa743c814700063f4bcf2631e
Саша <aleksa_kolesnik@mail.ru>
2022-03-10 19:28:05 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,44 +1,26 @@
-function getLowerNumber(from, to) {
-  return Math.min(Math.abs(from), Math.abs(to));
-}
+const getLowerNumber = (from, to) => Math.min(Math.abs(from), Math.abs(to));
 
-function getUpperNumber(from, to) {
-  return Math.max(Math.abs(from), Math.abs(to));
-}
+const getUpperNumber = (from, to) => Math.max(Math.abs(from), Math.abs(to));
 
-function getRandomNumber(from, to) {
+const getRandomNumber = (from, to) => {
   const lower = getLowerNumber(from, to);
   const upper = getUpperNumber(from, to);
   if (lower === upper) {
     return upper;
   }
   return Math.floor(Math.random() * (upper - lower + 1) + lower);
-}
+};
 
-function getRandomLocation(from, to, digit) {
+const getRandomLocation = (from, to, digit) => {
   const lower = getLowerNumber(from, to);
   const upper = getUpperNumber(from, to);
   if (lower === upper) {
     return upper;
   }
   return Math.round((Math.random() * (upper - lower) + lower) * (10 ** digit)) / (10 ** digit);
-}
+};
 
-const URLNUMBERS = [];
-for (let i = 0; i < 10; i++) {
-  if ((i + 1) >= 10) {
-    URLNUMBERS[i] = `${1 + i}` ;
-  } else {
-    URLNUMBERS[i] = `0${1 + i}`;
-  }
-}
-
-function getRandomUrl(numbers) {
-  const randomUrlindex = getRandomNumber(0, numbers.length - 1);
-  const randomUrl = numbers[randomUrlindex];
-  numbers.splice(randomUrlindex, 1);
-  return randomUrl;
-}
+const getNumberWithLeadZero = (num) => num < 10 ? `0${num}` : num;
 
 const TITLES = [
   '5-местная палатка',
@@ -91,22 +73,20 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-function getRandomArrayElements(elements) {
-  return elements[getRandomNumber(0, elements.length - 1)];
-}
+const getRandomArrayElements = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-function getMyltipleArrayElements(elements) {
-  return elements.slice(0, getRandomNumber(1, elements.length));
-}
+const getMyltipleArrayElements = (elements) =>  elements.slice(0, getRandomNumber(1, elements.length));
 
-function generateFakeData() {
+const generateFakeData = (value, index) => {
+  const lat = getRandomLocation(35.65, 35.70, 5);
+  const lng = getRandomLocation(139.7, 139.8, 5);
   return {
     autor: {
-      avatar: `img/avatars/user${getRandomUrl(URLNUMBERS)}.png`
+      avatar: `img/avatars/user${getNumberWithLeadZero(index + 1)}.png`
     },
     offer: {
       title: getRandomArrayElements(TITLES),
-      adress: `${getRandomLocation(35.65, 35.70, 5)},${getRandomLocation(139.7, 139.8, 5)}`,
+      adress: `${lat}, ${lng}`,
       price: getRandomNumber(100, 15000),
       type: getRandomArrayElements(TYPES),
       rooms: getRandomNumber(1, 5),
@@ -117,11 +97,12 @@ function generateFakeData() {
       photos: getMyltipleArrayElements(PHOTOS)
     },
     location: {
-      lat: getRandomLocation(35.65, 35.70, 5),
-      lng: getRandomLocation(139.7, 139.8, 5)
+      lat: lat,
+      lng: lng
     }
   };
-}
+};
 
-const odjectDataArray = Array.from({length: 10},generateFakeData);
-odjectDataArray;
+const odjectDataArray = Array.from({length: 10}, generateFakeData);
+
+export {odjectDataArray};
