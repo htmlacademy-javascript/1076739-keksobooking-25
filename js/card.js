8e829f633ff977d54f658f9543780da68b624833
Саша <aleksa_kolesnik@mail.ru>
2022-03-22 11:40:35 +0500

    Отрисуй меня полностью (часть 1)

--- js/card.js
+++ js/card.js
@@ -1,111 +1,86 @@
-import { odjectDataArray } from './data.js';
+import { generateOdjectDataArray, TYPES } from './data.js';
 
-const cardsDataArray = odjectDataArray();
+const cardsDataArray = generateOdjectDataArray();
 const cardTemplate = document.querySelector('#card').content.querySelector('article');
 const mapCanvas = document.querySelector('#map-canvas');
 const cardListFragment = document.createDocumentFragment();
 
-const popupTypeCompare = (type) => {
-  switch (type) {
-    case 'flat':
-      return 'Квартира';
-    case 'bungalow':
-      return 'Бунгало';
-    case 'house':
-      return 'Дом';
-    case 'palace':
-      return 'Дворец';
-    case 'hotel':
-      return 'Отель';
-    default:
-      return 'Хижина лесника';
-  }
+const createFeatures = (parentList, userFeatures) => {
+  parentList.innerHTML = '';
+  userFeatures.forEach((feature) => {
+    const newFeature = document.createElement('li');
+    newFeature.classList.add('popup__feature');
+    newFeature.classList.add(`popup__feature--${feature}`);
+    parentList.appendChild(newFeature);
+  });
 };
 
-const featuresCompare = (userArray, patternArray) => {
-  patternArray.forEach((patternElement) => {
+const createPhotos = (parentList, photosSrc) => {
+  parentList.innerHTML = '';
+  photosSrc.forEach((photoSrc) => {
+    const photoElement = document.createElement('img');
+    photoElement.width = 45;
+    photoElement.height = 40;
+    photoElement.src = photoSrc;
+    photoElement.alt = 'Фотография жилья';
+    photoElement.classList.add('popup__photo');
+    parentList.appendChild(photoElement);
+  });
+};
 
-    const isFeatures = userArray.some((element) =>
-      patternElement.classList.contains(`popup__feature--${element}`)
-    );
-    if (!isFeatures) {
-      patternElement.remove();
+const fillElement = (element, content, ...parts) => {
+  if (content) {
+    element.textContent = content;
+    if (parts) {
+      parts.forEach((part) => {
+        element.textContent += part;
+      });
     }
-  });
-};
-
-const photosCompare = (photos) => {
-  const photoListFragment = document.createDocumentFragment();
-  photos.forEach((photosElement) => {
-    const photo = document.createElement('img');
-    photo.width = 45;
-    photo.height = 40;
-    photo.src = photosElement;
-    photo.alt = 'Фотография жилья';
-    photo.classList.add('popup__photo');
-    photoListFragment.appendChild(photo);
-
-  });
-  return photoListFragment;
+  } else {
+    element.remove();
+  }
 };
 
 cardsDataArray.forEach((cards) => {
   const newCard = cardTemplate.cloneNode(true);
-  if (cards.offer.title) {
-    newCard.querySelector('.popup__title').textContent = cards.offer.title;
-  } else {
-    newCard.querySelector('.popup__title').style.display = 'none';
-  }
-  if (cards.offer.address) {
-    newCard.querySelector('.popup__text--address').textContent = cards.offer.address;
-  } else {
-    newCard.querySelector('.popup__text--address').style.display = 'none';
-  }
-  if (cards.offer.price) {
-    newCard.querySelector('.popup__text--price').textContent = `${cards.offer.price}  ₽/ночь`;
-  } else {
-    newCard.querySelector('.popup__text--price').textContent = 'Цена по запросу';
-  }
-  if (cards.offer.type) {
-    newCard.querySelector('.popup__type').textContent = popupTypeCompare(cards.offer.type);
-  } else {
-    newCard.querySelector('.popup__type').style.display = 'none';
-  }
-  if (cards.offer.rooms && cards.offer.guests) {
-    newCard.querySelector('.popup__text--capacity').textContent = `${cards.offer.rooms} комнаты для ${cards.offer.guests} гостей`;
-  } else {
-    newCard.querySelector('.popup__text--capacity').textContent = 'Есть номера различной вместимости';
-  }
+
+  fillElement(newCard.querySelector('.popup__title'), cards.offer.title);
+
+  fillElement(newCard.querySelector('.popup__text--address'), cards.offer.address);
+
+  fillElement(newCard.querySelector('.popup__text--price'), cards.offer.price, ' ₽/', 'ночь');
+
+  fillElement(newCard.querySelector('.popup__type'), TYPES[cards.offer.type]);
+
+  fillElement(newCard.querySelector('.popup__text--capacity'), cards.offer.rooms, ' комнаты для ', cards.offer.guests, ' гостей');
+
   if (cards.offer.checkin && cards.offer.checkout) {
     newCard.querySelector('.popup__text--time').textContent = `Заезд после ${cards.offer.checkin}, выезд до ${cards.offer.checkout}`;
   } else {
-    newCard.querySelector('.popup__text--time').style.display = 'none';
+    newCard.querySelector('.popup__text--time').remove();
   }
+
   if (cards.offer.description) {
-    featuresCompare(cards.offer.features, newCard.querySelectorAll('.popup__feature'));
+    createFeatures(newCard.querySelector('.popup__features'), cards.offer.features);
   } else {
-    newCard.querySelectorAll('.popup__feature').style.display = 'none';
+    newCard.querySelectorAll('.popup__feature').remove();
   }
-  if (cards.offer.description) {
-    newCard.querySelector('.popup__description').textContent = cards.offer.description;
+
+  fillElement(newCard.querySelector('.popup__description'), cards.offer.description);
+
+  if (cards.offer.photos) {
+    createPhotos(newCard.querySelector('.popup__photos'), cards.offer.photos);
   } else {
-    newCard.querySelector('.popup__description').style.display = 'none';
-  } if (cards.offer.photos) {
-    const photosList = newCard.querySelector('.popup__photos');
-    photosList.innerHTML = '';
-    photosList.appendChild(photosCompare(cards.offer.photos));
-  } else {
-    newCard.querySelector('.popup__photos').style.display = 'none';
+    newCard.querySelector('.popup__photos').remove();
   }
+
   if (cards.author.avatar) {
     newCard.querySelector('.popup__avatar').src = cards.author.avatar;
-  }
-  else {
-    newCard.querySelector('.popup__avatar').style.display = 'none';
+  } else {
+    newCard.querySelector('.popup__avatar').remove();
   }
 
   cardListFragment.appendChild(newCard);
 });
 
 mapCanvas.appendChild(cardListFragment.querySelectorAll('article')[0]);
-
